3d1af7e7e670373717a658894e15864d013e81ff
tfcprivt <141457761+privt00@users.noreply.github.com>
2024-04-15 18:57:46 +0200

    Update inbox.js

--- api/user/inbox.js
+++ api/user/inbox.js
@@ -4,13 +4,18 @@ const express = require("express");
 module.exports.load = async function (app, db) {
     app.get("/api/inbox", async (req, res) => {
         try {
-            const userId = req.session.pterodactyl.username;
+            if (!req.session.pterodactyl || !req.session.pterodactyl.username) {
+                return res.status(401).json({ error: 'Unauthorized' });
+            }
 
-            const alluserId = '@a';
+            const userId = req.session.pterodactyl.username;
+            const allUserId = '@a';
+
             const inboxData = JSON.parse(fs.readFileSync("./inbox.json", "utf8"));
             const userMessages = inboxData.messages.filter(message => message.toUserId === userId);
-            const userMessages1 = inboxData.messages.filter(message => message.toUserId === alluserId);
-            res.json({ messages: userMessages,messages: userMessages1 });
+            const allUserMessages = inboxData.messages.filter(message => message.toUserId === allUserId);
+
+            res.json({ userMessages, allUserMessages });
         } catch (error) {
             console.error('Error loading inbox data:', error);
             res.status(500).json({ error: 'Internal Server Error' });
